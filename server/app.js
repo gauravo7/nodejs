875e4aab59178c811bb1dfdc5035f77cc455d17c
Gaurav Sharma <gaurav@o7services.com>
2024-08-13 09:52:50 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 
-// const db = require("./config/db")
+const db = require("./config/db")
 app.use(express.urlencoded({extended:true}))
 
 const adminroutes = require("./routes/adminroutes")
-// app.use("/admin",adminroutes)
+app.use("/admin",adminroutes)
 
 app.get("/",function(req,res){
     res.send("<h2>Welcome to APP JS Page to Show Data</h2>");
