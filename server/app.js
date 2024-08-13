5781cb9d97f743e63b6307472e87e3ebccf0b28c
Gaurav Sharma <gaurav@o7services.com>
2024-08-13 09:38:19 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const app = express()
 
-const db = require("./config/db")
+// const db = require("./config/db")
 app.use(express.urlencoded({extended:true}))
 
 const adminroutes = require("./routes/adminroutes")
-app.use("/admin",adminroutes)
+// app.use("/admin",adminroutes)
 
 app.get("/",function(req,res){
-    console.log("Welcome to API")
+    res.send("<h2>Welcome to APP JS Page to Show Data</h2>");
 })
-let seed= require('./common/seed')
+// let seed= require('./common/seed')
 
 app.listen(3000,function(){
     console.log("Server Running at 3000")
